d272a5b1ff6207e3c6ef864d7364de52e6ae747b
Alicia Cadorin <yourname@examplecfw= gtew=.com>
2023-01-09 19:54:51 -0500

    js edits, css edits

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
+
+const title = document.querySelector('.title');
+const userButtons = document.querySelector('.user-buttons');
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('scissors');
+
+
+
 let choice1 = prompt (' pick one: rock, paper, scissors!');
+
+let userScore = 0;
+let computerScore = 0;
+
 
 let choice2 =  Math.floor(Math.random()*3);
 if ( choice2 === 1) {
@@ -18,7 +31,7 @@ let game = (choice1, choice2) => {
         return 'scissors wins.';
     }
 
-     if ( choice1 === 'rock' && choice2==='scissors'){
+     if ( choice1 === 'rock' && choice2 ==='scissors'){
         return ' Rock defeats scissors. woo!';
     } else if (choice2 === 'paper') {
         return 'paper wins';
@@ -29,6 +42,23 @@ let game = (choice1, choice2) => {
         return 'rock wins';
      }
     };
+
+    let checkWinner = () => {
+
+        if (userScore === 5){
+            return 'User Wins';
+        }else if (computerScore === 5) {
+            return 'Computer Wins';
+        } else {
+            return 'tie';
+        }
+
+    }
+
+    
+
+    game();
+    console.log(game());
 
     var dt = new Date();
     document.getElementById('date-time').innerHTML=dt;
